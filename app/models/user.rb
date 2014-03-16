240e39ec64ae9eb6e808bb0fffdfc7fd91ed69e2
Cevaris <cevaris@gmail.com>
2014-03-16 14:52:20 -0600

    replaced image count

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,6 @@ class User < ActiveRecord::Base
   }
 
   # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, :content_type => %w(image/jpeg image/jpg image/png)
 end
