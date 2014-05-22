ca0dc8ff5bc68a9fb25be8cbbb0a5c666ca78518
Cevaris <cevaris@gmail.com>
2014-05-22 13:49:21 -0600

    added reprocess ors

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,11 +1,13 @@
 class Photo < ActiveRecord::Base
   belongs_to :photo_session
 
-  has_attached_file :image, :styles => {
-    thumb: '100x100#', 
-    square: '200x200#', 
-    medium: '500x500>',
-    large: '700x700>' }
+  has_attached_file :image, 
+    :styles => {
+      thumb: '100x100#', 
+      square: '200x200#', 
+      medium: '500x500>',
+      large: '800x800>',
+      xlarge: '1600x1400>' }
 
   validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 
