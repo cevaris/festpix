2f646be35790ec013cd5be0a6063e45e87038fbf
Cevaris <cevaris@gmail.com>
2014-05-29 16:12:33 -0600

    removed commented stale code

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-# User.find_by_email('festpix@gmail.com').delete
-# User.new(
-#   email:  'festpix@gmail.com',
-#   password: ENV['ADMIN_PASS'],
-#   password_confirmation: ENV['ADMIN_PASS'],
-#   encrypted_password: ENV['ADMIN_PASS'],
-#   phone_number: '5594516126' 
-# ).save!
-
-
-# User.find_by_email('festpix@gmail.com').delete
-# User.create(
-#   email: 'festpix@gmail.com',
-#   password: ENV['ADMIN_PASS'],
-#   password_confirmation: ENV['ADMIN_PASS'],
-#   phone_number: '5594516126')
-
 User.find_or_create_by_email!(
   email: 'festpix@gmail.com',
   password: ENV['ADMIN_PASS'],
