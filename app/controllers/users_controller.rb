31776d632a3309b68a1dc8904c814e3bf267fd40
Cevaris <cevaris@gmail.com>
2014-05-29 17:39:45 -0600

    removed 8 limiter for users profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,24 @@
 class UsersController < ApplicationController
 
   def index
-    redirect_to root_path    
+    unless current_user
+      redirect_to root_path
+    else
+      redirect_to current_user    
+    end
+    
   end
   
   def show
     @user = User.find_by_slug(params[:id])
     @sessions = PhotoSession.tagged_with(@user.phone_number, :any => true)
     
-    begin
-      friend_emails = @sessions.collect {|p| p.email_list }.flatten.uniq.select {|email| email if email != @user.email}
-      @friends = User.find(:all, {email:  friend_emails}, limit: 4)
-    rescue ActiveRecord::RecordNotFound
-      @friends = [] 
-    end
+    # begin
+    #   friend_emails = @sessions.collect {|p| p.email_list }.flatten.uniq.select {|email| email if email != @user.email}
+    #   @friends = User.find(:all, {email:  friend_emails}, limit: 4)
+    # rescue ActiveRecord::RecordNotFound
+    #   @friends = [] 
+    # end
 
     
   end
