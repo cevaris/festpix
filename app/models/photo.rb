c68476395e3a934f8dd2cb67363c4fef9a5d30ef
Cevaris <cevaris@gmail.com>
2014-06-01 13:55:39 -0600

    added env variable to toggle background processing

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,34 +2,52 @@ require 'securerandom'
 
 
 class Photo < ActiveRecord::Base
+
+  STYLES={
+    thumb: '100x100#', 
+    square: '200x200#', 
+    medium: '500x500>',
+    large: '800x800>',
+    xlarge: { 
+      :geometry => '1600x1400>', 
+      :watermark_path => "#{Rails.root}/public/watermarks/festpix.png"},
+      :transparancy => '30'
+  }
   belongs_to :photo_session
 
-  has_attached_file :image, 
+
+  if ENV['BACKGROUND_PROCESSING']=='true'
+    Rails.logger.info "Background Processing Enabled: #{ENV['BACKGROUND_PROCESSING']}"
+    has_attached_file :image, 
     :processors => [:watermark],
+    :styles => STYLES, 
+    :only_process => [:square, :xlarge]
+    
+    self.process_in_background :image, :only_process => [:thumb, :medium, :large]
+  else
+    Rails.logger.info "Background Processing Disabled: #{ENV['BACKGROUND_PROCESSING']}"
+    has_attached_file :image, 
+    :processors => [:watermark],
+    :styles => STYLES
+  end
+
+  # self.process_in_background :image, :only_process => BACKGROUND_STYLES
+
+  # has_attached_file :image, 
+  #   :processors => [:watermark],
+  #   :styles => STYLES
     # :convert_options => {
     #   :xlarge => {
     #     :transparancy => '15'
     #   }
     # },
-    :styles => {
-      thumb: '100x100#', 
-      square: '200x200#', 
-      medium: '500x500>',
-      large: '800x800>',
-      xlarge: { 
-        :geometry => '1600x1400>', 
-        :watermark_path => "#{Rails.root}/public/watermarks/festpix.png"},
-        :transparancy => '30'
-    }#, :only_process => [:square, :xlarge]
+    #, :only_process => [:square, :xlarge]
 
   validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 
   # process_in_background :image
   # process_in_background :image, :only_process => [:thumb, :medium, :large]
 
-  def url
-    Rails.application.routes.url_helpers.photo_short_url(self, host: ENV['SHORT_URL'])
-  end
   def short_url
     Rails.application.routes.url_helpers.photo_short_url(self, host: ENV['SHORT_URL'])[7..-1]
   end
