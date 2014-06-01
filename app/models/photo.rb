c41daea17b00c84d803bc935322efee0ac3938b9
Cevaris <cevaris@gmail.com>
2014-06-01 14:14:22 -0600

    cleaned up photo class

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,6 +3,9 @@ require 'securerandom'
 
 class Photo < ActiveRecord::Base
 
+
+  belongs_to :photo_session
+
   STYLES={
     thumb: '100x100#', 
     square: '200x200#', 
@@ -13,9 +16,7 @@ class Photo < ActiveRecord::Base
       :watermark_path => "#{Rails.root}/public/watermarks/festpix.png"},
       :transparancy => '30'
   }
-  belongs_to :photo_session
-
-
+  
   if ENV['BACKGROUND_PROCESSING']=='true'
     Rails.logger.info "Background Processing Enabled: #{ENV['BACKGROUND_PROCESSING']}"
     has_attached_file :image, 
@@ -31,22 +32,8 @@ class Photo < ActiveRecord::Base
     :styles => STYLES
   end
 
-  # self.process_in_background :image, :only_process => BACKGROUND_STYLES
-
-  # has_attached_file :image, 
-  #   :processors => [:watermark],
-  #   :styles => STYLES
-    # :convert_options => {
-    #   :xlarge => {
-    #     :transparancy => '15'
-    #   }
-    # },
-    #, :only_process => [:square, :xlarge]
-
   validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 
-  # process_in_background :image
-  # process_in_background :image, :only_process => [:thumb, :medium, :large]
 
   def url
     Rails.application.routes.url_helpers.photo_short_url(self, host: ENV['SHORT_URL'])
