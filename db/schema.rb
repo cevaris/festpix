b38b64e4cb5507016f5a43f5e2117a558b6d529e
Adam Cardenas <cevaris@gmail.com>
2014-09-27 08:31:13 -0600

    added customer to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922022534) do
+ActiveRecord::Schema.define(version: 20140927140909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(version: 20140922022534) do
     t.string   "phone_number"
     t.string   "slug"
     t.string   "role"
+    t.integer  "customer_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user='admin@festpix.com'
-User.find_or_initialize_by_email(user).update_attributes({
-  email: user,
-  password: ENV['ADMIN_PASS'],
-  phone_number: '5594516126',
-  role: User::ROLES[:super_admin]
-})
-
-
 ## Add default Festpix Event/Customer to all previous photo sessions
+user        = 'admin@festpix.com'
 fp_customer = 'Festpix'
+
 puts Customer.find_or_initialize_by_name(fp_customer).update_attributes({
   slug:        'festpix',
   color_one:   '#1b1b24',
   color_two:   '#333333',
   color_three: '#428bca',
+})
+
+puts User.find_or_initialize_by_email(user).update_attributes({
+  email: user,
+  password: ENV['ADMIN_PASS'],
+  phone_number: '5594516126',
+  role: User::ROLES[:super_admin],
+  customer_id: Customer.find_by_name(fp_customer).id,
 })
 
 puts Event.find_or_initialize_by_name(fp_customer).update_attributes({
@@ -32,5 +33,5 @@ puts Event.find_or_initialize_by_name(fp_customer).update_attributes({
   facebook_text: 'Shared via',
   twitter_url:   'https://twitter.com/festpix',
   twitter_text:  'Shared via',
-  customer_id:   Customer.find_by_name(fp_customer).id
+  customer_id:   Customer.find_by_name(fp_customer).id,
 })
