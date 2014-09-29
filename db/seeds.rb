b4436a7124dba740ac702019c0eb480b5ef62055
Adam Cardenas <cevaris@gmail.com>
2014-09-28 18:51:40 -0600

    updated seed to add admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,49 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 ## Add default Festpix Event/Customer to all previous photo sessions
-user        = 'admin@festpix.com'
-fp_customer = 'Festpix'
+super_admin_email = 'admin@festpix.com'
+fp_customer_name = 'Festpix'
+fp_customer_slug = 'festpix'
+admins = [
+  'mark@festpix.com',
+  'alex@festpix.com',
+  'colton@festpix.com',
+]
 
-puts Customer.find_or_initialize_by_name(fp_customer).update_attributes({
-  slug:        'festpix',
+def create_admin(email, customer_slug)
+  puts email, User.find_or_initialize_by_email(email).update_attributes({
+    email: email,
+    password: ENV['ADMIN_PASS'],
+    role: User::ROLES[:admin],
+    customer_id: Customer.find_by_slug(customer_slug).id,
+  })
+end
+
+puts Customer.find_or_initialize_by_slug(fp_customer_slug).update_attributes({
+  name:        fp_customer_name,
   color_one:   '#1b1b24',
   color_two:   '#333333',
   color_three: '#428bca',
 })
 
-puts User.find_or_initialize_by_email(user).update_attributes({
-  email: user,
+puts super_admin_email, User.find_or_initialize_by_email(super_admin_email).update_attributes({
+  email: super_admin_email,
   password: ENV['ADMIN_PASS'],
   phone_number: '5594516126',
   role: User::ROLES[:super_admin],
-  customer_id: Customer.find_by_name(fp_customer).id,
+  customer_id: Customer.find_by_slug(fp_customer_slug).id,
 })
 
-puts Event.find_or_initialize_by_name(fp_customer).update_attributes({
-  slug:          'festpix',
+puts Event.find_or_initialize_by_slug(fp_customer_slug).update_attributes({
+  name:          fp_customer_name,
   logo:          File.new("#{Rails.root}/public/watermarks/festpix.png"),
   sms_text:      'FestPix! Your images are ready, click the link to see them.',
   facebook_url:  'https://www.facebook.com/festpix',
   facebook_text: 'Shared via',
   twitter_url:   'https://twitter.com/festpix',
   twitter_text:  'Shared via',
-  customer_id:   Customer.find_by_name(fp_customer).id,
+  customer_id:   Customer.find_by_slug(fp_customer_slug).id,
 })
+
+admins.each {|a| create_admin(a, fp_customer_slug)}
+
