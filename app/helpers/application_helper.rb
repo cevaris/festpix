2cbb0b6d82a436bd63a4cd8142414e9730cfac12
Adam Cardenas <cevaris@gmail.com>
2015-02-24 19:24:52 -0700

    updated production check logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
 
   def is_production?
-    ENV['HOST_URL'].include?('festpix.herokuapp.com') || ENV['HOST_URL'].include?('app.festpix.com')
+    ENV['HOST_URL'].include?('festpix.herokuapp.com') || ENV['HOST_URL'] == 'http://www.festpix.com'
   end
 
 end
